d6e79af5c728cdfe02483071e3f67005f8ddbd20
BadgeSWS <jackson.brajer@gmail.com>
2020-01-27 20:49:34 -0500

    Thread interrupting and restarting

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,32 @@
-import subsystems.SubsystemManager;
 import threads.LoopedThread;
-
-import java.util.Scanner;
 
 public class Main {
 
     public static void main (String[] args) throws InterruptedException {
 
-        SubsystemManager sm = SubsystemManager.getInstance();
+        LoopedThread lt = new LoopedThread(0.5) {
+            @Override
+            public void update(double dt) {
+                System.out.println(dt);
+                try {
+                    Thread.sleep(4* 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("After");
+            }
+        };
 
-        sm.start();
-
-        Scanner sc = new Scanner(System.in);
-        sc.nextLine();
+        lt.start();
+        Thread.sleep(3 * 1000);
+        System.out.println("Stopped Thread");
+        lt.interrupt();
+        Thread.sleep(3 * 1000);
+        System.out.println("Started Thread");
+        lt.start();
+        Thread.sleep(3 * 1000);
+        System.out.println("Stopped Thread");
+        lt.stop();
     }
 
 }
